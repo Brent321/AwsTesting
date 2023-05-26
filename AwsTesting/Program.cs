7ab1dcbad89ee49e9680995e3cdf9fc234b50b96
brent sieberg <siebergb@gmail.com>
2023-05-26 19:01:18 -0400

    Adding cloudwatch nuget packages and logging program.cs config.

--- AwsTesting/Program.cs
+++ AwsTesting/Program.cs
@@ -1,3 +1,7 @@
+using Amazon.CloudWatchLogs;
+using Serilog;
+using Serilog.Sinks.AwsCloudWatch;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var clientConfig = new AmazonCloudWatchLogsConfig()
+{
+    ServiceURL = "http://localstack:4566",
+    UseHttp = true
+};
+var client = new AmazonCloudWatchLogsClient(clientConfig);
 
+var log = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.Console()
+    .WriteTo.AmazonCloudWatch(
+        logGroup: "test",
+        logStreamPrefix: DateTime.UtcNow.ToString("yyyyMMddHHmmssfff"),
+        cloudWatchClient: client)
+    .CreateLogger();
+Log.Logger = log;
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
