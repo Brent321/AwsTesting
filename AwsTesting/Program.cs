3ac1320a6506e57ff9e1bb7af47631880a44b9f4
brent sieberg <siebergb@gmail.com>
2023-05-27 19:02:41 -0400

    App builds after setting up secrets manager. Secrets manager functionality not tested.

--- AwsTesting/Program.cs
+++ AwsTesting/Program.cs
@@ -1,4 +1,5 @@
 using Amazon.CloudWatchLogs;
+using Amazon.SecretsManager;
 using AwsTesting;
 using Serilog;
 using Serilog.Sinks.AwsCloudWatch;
@@ -7,27 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Host.UseSerilog();
+
+builder.SetupSecretManager();
+builder.SetupLogging();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var clientConfig = new AmazonCloudWatchLogsConfig()
-{
-    ServiceURL = "http://localstack:4566",
-    UseHttp = true
-};
-var client = new AmazonCloudWatchLogsClient(clientConfig);
 
-var log = new LoggerConfiguration()
-    .MinimumLevel.Verbose()
-    .WriteTo.Console()
-    .WriteTo.AmazonCloudWatch(
-        logGroup: "test",
-        logStreamPrefix: DateTime.UtcNow.ToString("yyyyMMddHHmmssfff"),
-        createLogGroup: true,
-        cloudWatchClient: client)
-    .CreateLogger();
-Log.Logger = log;
 builder.Services.AddServices();
 var app = builder.Build();
 
--- AwsTesting/ServiceCollectionExtensions.cs
+++ AwsTesting/ServiceCollectionExtensions.cs
@@ -1,4 +1,8 @@
-﻿using AwsTesting.BackgroundServices;
+﻿using Amazon.CloudWatchLogs;
+using Amazon.SecretsManager;
+using AwsTesting.BackgroundServices;
+using Serilog;
+using Serilog.Sinks.AwsCloudWatch;
 
 namespace AwsTesting
 {
@@ -7,6 +11,42 @@ namespace AwsTesting
         public static void AddServices(this IServiceCollection services)
         {
             services.AddHostedService<Worker>();
+        }
+
+        public static void SetupSecretManager(this WebApplicationBuilder builder)
+        {
+            builder.Configuration.AddSecretsManager(configurator: options =>
+            {
+                var config = new AmazonSecretsManagerConfig()
+                {
+                    ServiceURL = "http://localstack:4566",
+                    UseHttp = true
+                };
+                options.CreateClient = () => new AmazonSecretsManagerClient(config);
+            });
+        }
+
+        public static void SetupLogging(this WebApplicationBuilder builder)
+        {
+            builder.Host.UseSerilog();
+            
+            var clientConfig = new AmazonCloudWatchLogsConfig()
+            {
+                ServiceURL = "http://localstack:4566",
+                UseHttp = true
+            };
+            var client = new AmazonCloudWatchLogsClient(clientConfig);
+
+            var log = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .WriteTo.AmazonCloudWatch(
+                    logGroup: "test",
+                    logStreamPrefix: DateTime.UtcNow.ToString("yyyyMMddHHmmssfff"),
+                    createLogGroup: true,
+                    cloudWatchClient: client)
+                .CreateLogger();
+            Log.Logger = log;
         }
     }
 }
