2d8015a798aa93f24c613ceb807e8470878e3bc9
brent sieberg <siebergb@gmail.com>
2023-05-28 15:49:54 -0500

    Cleaning up formatting

--- AwsTesting/BackgroundServices/Worker.cs
+++ AwsTesting/BackgroundServices/Worker.cs
@@ -6,22 +6,21 @@ namespace AwsTesting.BackgroundServices
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private readonly ConnectionStrings _config;
+        private readonly ConnectionStrings _connectionStrings;
 
         public Worker(
             ILogger<Worker> logger,
-            IOptions<ConnectionStrings> config)
+            IOptions<ConnectionStrings> connectionStrings)
         {
             _logger = logger;
-            _config = config.Value;
+            _connectionStrings = connectionStrings.Value;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Secret: {x}", _config.Mssql);
+                _logger.LogInformation("Secret: {x}", _connectionStrings.Mssql);
                 await Task.Delay(2000, stoppingToken);
             }
         }
